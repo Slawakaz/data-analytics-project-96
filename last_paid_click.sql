f6a0214a693d60d9939a0803c3a519f5229603b2
Slawakaz <v.kazachenok1996@gmail.com>
2025-05-17 22:18:38 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,24 +1,41 @@
-select 
+with tab1 as(SELECT 
     s.visitor_id,
     s.visit_date,
-    s.source as utm_source,
-    s.medium as utm_medium,
-    s.campaign as utm_campaign,
+    ROW_NUMBER() OVER (
+            PARTITION BY s.visitor_id 
+            ORDER BY s.visit_date DESC
+        ) AS rn,
+    s.source AS utm_source,
+    s.medium AS utm_medium,
+    s.campaign AS utm_campaign,
     l.lead_id,
     l.created_at,
-    sum(coalesce(l.amount, 0)) as amount,
+    sum(COALESCE(l.amount, 0)) AS amount,
     l.closing_reason,
     l.status_id
-from 
+FROM 
     sessions s
-left join 
-    leads l using (visitor_id)
-where 
-    s.source in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social') or  s.medium  in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    group by 1,2,3,4,5,6,7,9,10
-order by 
-    amount desc nulls last,
-    s.visit_date,
+LEFT JOIN 
+    leads l USING (visitor_id)
+WHERE   s.medium  IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social') 
+    group by 1,2,4,5,6,7,8,10,11
+    )
+    SELECT 
+    visitor_id,
+    visit_date,
+   utm_source,
+   utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+     amount,
+    closing_reason,
+    status_id
+FROM tab1
+where rn = 1
+ORDER BY 
+    amount DESC NULLS LAST,
+    visit_date,
     utm_source,
     utm_medium,
     utm_campaign
