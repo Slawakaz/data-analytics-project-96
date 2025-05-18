620f24f1b0ea26a9b344d1b4a71fbdc6ad09520d
Slawakaz <v.kazachenok1996@gmail.com>
2025-05-18 19:00:34 +0300

    Update Last Paid Click aggregate_last_paid_click.sql

--- Last Paid Click aggregate_last_paid_click.sql
+++ Last Paid Click aggregate_last_paid_click.sql
@@ -1,27 +1,39 @@
-with tab1 as (
-select utm_source,  utm_medium, utm_campaign, sum(daily_spent) as total_cost
-from ya_ads
-group by 1,2,3
-union all
- select utm_source,  utm_medium, utm_campaign, sum(daily_spent) as total_cost
-from vk_ads
-group by 1,2,3
+WITH last_paid_clicks AS (    select  visitor_id,  visit_date::date AS visit_date, source AS utm_source,
+     medium AS utm_medium,  campaign AS utm_campaign,  l.lead_id, l.amount,
+        CASE 
+            WHEN l.closing_reason = 'Успешно реализовано' OR l.status_id = 142 
+            THEN 1 ELSE 0 
+        END AS purchases,
+        ROW_NUMBER() OVER (
+            PARTITION BY visitor_id 
+            ORDER BY visit_date DESC
+        ) AS rn
+    FROM sessions
+   LEFT JOIN leads l using(visitor_id) 
+    WHERE medium IN ('cpc','cpm','cpa','youtube','cpp','tg','social')
+),
+combined_ads AS (
+  select  utm_source,  utm_medium, utm_campaign, SUM(daily_spent) AS total_spent
+    FROM (
+        SELECT  utm_source, utm_medium, utm_campaign, daily_spent
+        FROM vk_ads
+        UNION ALL
+        SELECT   utm_source,  utm_medium,  utm_campaign,  daily_spent
+        FROM ya_ads
+    ) AS ads
+    GROUP BY utm_source, utm_medium, utm_campaign
 )
-select to_char(max(visit_date), 'yyyy-mm-dd') as visit_date ,
-s.source as utm_source, s.medium as utm_medium, s.campaign as utm_campaign ,
-count(s.visitor_id) as visitors_count, 
-t1.total_cost, 
-count (l.lead_id ) as leads_count,
-sum(case  when l.closing_reason = 'Успешно реализовано' or l.status_id = 142 then 1 
-  else 0 
-  end )as is_purchase,
-sum (l.amount) as revenue 
-from  sessions s
-left join leads l on l.visitor_id = s.visitor_id
-join tab1 t1 on s.medium=t1.utm_medium and s.source=t1.utm_source and 
-s.campaign=t1.utm_campaign
-group by 2,3,4,6
-order by
-    revenue desc nulls last, visit_date, visitors_count  desc, 
-    utm_source, utm_medium, utm_campaign
- limit 15;
+SELECT
+  lpc.visit_date,
+   COUNT(DISTINCT lpc.visitor_id) AS visitors_count,
+  lpc.utm_source, lpc.utm_medium,  lpc.utm_campaign,  ca.total_spent as total_cost, 
+  COUNT(distinct lpc.lead_id) AS leads_count, SUM(coalesce( purchases,0)) as purchases_count ,
+       sum( coalesce (amount, 0)) as  revenue
+    FROM last_paid_clicks lpc
+    LEFT JOIN combined_ads ca  ON lpc.utm_source = ca.utm_source  AND lpc.utm_medium = ca.utm_medium
+    AND lpc.utm_campaign = ca.utm_campaign
+    WHERE lpc.rn = 1 
+    GROUP BY lpc.visit_date, lpc.utm_source, lpc.utm_medium, lpc.utm_campaign, ca.total_spent
+ORDER BY
+    revenue DESC NULLS LAST,  visit_date ASC,  visitors_count DESC,  utm_source, utm_medium,  utm_campaign
+    limit 15;
