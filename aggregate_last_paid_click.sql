2e734b8789b844793db00df1f73cfe3be9b4aba7
Slawakaz <v.kazachenok1996@gmail.com>
2025-05-19 13:26:58 +0300

    Create aggregate_last_paid_click.sql

---
+++ aggregate_last_paid_click.sql
@@ -0,0 +1,40 @@
+with last_paid_clicks as (    select  s.visitor_id,  date(visit_date) as visit_date, source as utm_source,
+     medium as utm_medium,  campaign as utm_campaign,  l.lead_id, l.amount,
+        case 
+            when l.closing_reason = 'успешно реализовано' or l.status_id = 142 
+            then 1 else 0 
+        end as purchases,
+        row_number() over (
+            partition by s.visitor_id 
+            order by visit_date desc
+        ) as rn
+    from sessions s 
+   left join leads l on s.visitor_id = l.visitor_id
+        and l.created_at >= s.visit_date  
+    where medium in ('cpc','cpm','cpa','youtube','cpp','tg','social')  
+),
+combined_ads as (
+  select  utm_source,  utm_medium, utm_campaign, date(campaign_date) as campaign_date, sum(daily_spent) as total_spent
+    from (
+        select  utm_source, utm_medium, utm_campaign, daily_spent, campaign_date
+        from vk_ads
+        union all
+        select   utm_source,  utm_medium,  utm_campaign,  daily_spent, campaign_date
+        from ya_ads
+    ) as ads
+    group by utm_source, utm_medium, utm_campaign, campaign_date
+)
+select
+  lpc.visit_date,
+   count(distinct lpc.visitor_id) as visitors_count,
+  lpc.utm_source, lpc.utm_medium,  lpc.utm_campaign,  ca.total_spent as total_cost, 
+  count(distinct lpc.lead_id) as leads_count, sum(coalesce( purchases,0)) as purchases_count ,
+       sum( coalesce (amount, 0)) as  revenue
+    from last_paid_clicks lpc
+    left join combined_ads ca  on lpc.utm_source = ca.utm_source  and lpc.utm_medium = ca.utm_medium 
+    and lpc.utm_campaign = ca.utm_campaign and lpc.visit_date = ca.campaign_date
+    where lpc.rn = 1 
+    group by lpc.visit_date, lpc.utm_source, lpc.utm_medium, lpc.utm_campaign, ca.total_spent
+order by
+    revenue desc nulls last,  visit_date asc,  visitors_count desc,  utm_source, utm_medium,  utm_campaign
+    limit 15;
