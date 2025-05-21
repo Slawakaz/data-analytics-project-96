bfb2a6a6ec275a0c25d2b32c2fd6b8cef0d737f2
Slawakaz <v.kazachenok1996@gmail.com>
2025-05-21 13:13:49 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,42 +1,46 @@
-with tab1 as(select 
-    s.visitor_id,
-    s.visit_date,
-    row_number() over (
-            partition by s.visitor_id 
+with tab1 as (
+    select
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        row_number() over (
+            partition by s.visitor_id
             order by s.visit_date desc
         ) as rn,
-    s.source as utm_source,
-    s.medium as utm_medium,
-    s.campaign as utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-from 
-    sessions s
-left join 
-    leads l using (visitor_id)
-where   s.medium  in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social') and l.created_at  >= s.visit_date 
-    )
-    select 
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    from sessions as s
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and l.created_at >= s.visit_date
+    where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+)
+
+select
     visitor_id,
     visit_date,
-   utm_source,
-   utm_medium,
+    utm_source,
+    utm_medium,
     utm_campaign,
     lead_id,
     created_at,
-     amount,
+    amount,
     closing_reason,
     status_id
 from tab1
 where rn = 1
-order by 
+order by
     amount desc nulls last,
-    visit_date,
-    utm_source,
-    utm_medium,
-    utm_campaign
-    limit 10;
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc
+limit 10;
+
 
