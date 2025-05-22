ce7003f2d0c500c4fca93c21552734c6593079be
Slawakaz <v.kazachenok1996@gmail.com>
2025-05-22 20:22:40 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -11,15 +11,7 @@ SELECT
     medium,
     campaign,
     visit_date::DATE AS visit_date,
-    CASE
-        WHEN EXTRACT(ISODOW FROM visit_date) = 1 THEN '1monday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 2 THEN '2tuesday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 3 THEN '3wednesday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 4 THEN '4thursday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 5 THEN '5friday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 6 THEN '6saturday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 7 THEN '7sunday'
-    END AS day_of_week,
+    TO_CHAR(visit_date, 'IDDay'),
     COUNT(DISTINCT visitor_id) AS visitor_count
 FROM sessions
 GROUP BY
@@ -27,15 +19,7 @@ GROUP BY
     medium,
     campaign,
     visit_date::DATE,
-    CASE
-        WHEN EXTRACT(ISODOW FROM visit_date) = 1 THEN '1monday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 2 THEN '2tuesday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 3 THEN '3wednesday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 4 THEN '4thursday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 5 THEN '5friday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 6 THEN '6saturday'
-        WHEN EXTRACT(ISODOW FROM visit_date) = 7 THEN '7sunday'
-    END;
+    TO_CHAR(visit_date, 'IDDay');
 WITH tab1 AS (
     SELECT
         campaign_date,
@@ -127,7 +111,7 @@ tab2 AS (
         SUM(
             CASE
                 WHEN
-                    l.closing_reason = 'успешно реализовано'
+                    l.closing_reason = 'Успешно реализовано'
                     OR l.status_id = 142
                     THEN 1
                 ELSE 0
@@ -219,7 +203,7 @@ tab2 AS (
         SUM(
             CASE
                 WHEN
-                    l.closing_reason = 'успешно реализовано'
+                    l.closing_reason = 'Успешно реализовано'
                     OR l.status_id = 142
                     THEN 1
                 ELSE 0
