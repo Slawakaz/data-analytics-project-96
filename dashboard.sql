0caf61300bc3d02e74046be79125b4ef46a194a7
Slawakaz <v.kazachenok1996@gmail.com>
2025-05-22 15:55:58 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -66,3 +66,26 @@ GROUP BY
     utm_medium,
     utm_campaign,
     TO_CHAR(campaign_date, 'yyyy-mm-dd');
+with tab1 as (
+    select 
+        visitor_id,
+        min(visit_date) as first_visit_date 
+    from sessions
+    where source in ('vk', 'yandex')
+    group by visitor_id
+),
+tab2 as (
+    select 
+        l.visitor_id,
+        l.created_at as lead_closed_date,
+        t1.first_visit_date,
+        (l.created_at- t1.first_visit_date) as days_to_close
+    from leads l
+    join tab1 t1 using(visitor_id)
+    where l.closing_reason is not null  and l.created_at >= t1.first_visit_date
+    order by 4
+)
+select 
+    extract(day from percentile_cont(0.9) within group (order by days_to_close)) as percentile_90_days
+from tab2;
+
